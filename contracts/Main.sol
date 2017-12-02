60e4a244969edea683cdd7ad73c327acbac7a3cf
Tirthankar Bhowmick <tbhowmick@equilar.com>
2017-12-02 08:16:49 -0800

    compiling fine after claimAuthman

--- contracts/Main.sol
+++ contracts/Main.sol
@@ -1,3 +1,5 @@
+//TODO:replace requestedById/Name with msg.sender, Another mapping for authmans by sender
+
 pragma solidity ^0.4.11;
 
 import "./helper/strings.sol";
@@ -6,6 +8,8 @@ import "./helper/SsnRegex.sol";
 import "./helper/DobRegex.sol";
 import "./helper/PhoneRegex.sol";
 import "./helper/PinRegex.sol";
+import "./helper/LowerAlphanumericStartsWithAlphabetRegex.sol";
+import "./helper/LowerAlphanumericRegex.sol";
 
 
 contract Main
@@ -16,6 +20,8 @@ contract Main
   address dobRegex;
   address phoneRegex;
   address pinRegex;
+  address lowerAlphaStartsWithAlphaRegex;
+  address lowerAlphaRegex;
 
   struct Authman {
     bytes32 guid;
@@ -34,7 +40,6 @@ contract Main
     bool isClaimed;
     uint claimedDate;
     bytes32 claimedAuthmanIdHash;
-    bytes32 previousGuid;
   }
 
   // Used for error handling.
@@ -44,41 +49,43 @@ contract Main
   event Info(string message);
   event InfoHash(string message, bytes32 hash);
   event InfoAuthman1(
-          string message, 
-          bytes32 guid,
-          string firstName,
-          string lastName,
-          uint counter,
-          bytes32 ssnHash,
-          bytes32 claimHash,
-          string mobilePhone);
+    string message, 
+    bytes32 guid,
+    string firstName,
+    string lastName,
+    uint counter,
+    bytes32 ssnHash,
+    bytes32 claimHash,
+    string mobilePhone);
 
-   event InfoAuthman2(
-          string createByName,
-          uint createDate,
-          string updateByName,
-          uint updateDate,
-          uint claimedDate,
-          bytes32 claimedAuthmanIdHash,
-          bytes32 previousGuid);
+  event InfoAuthman2(
+    string createByName,
+    uint createDate,
+    string updateByName,
+    uint updateDate,
+    uint claimedDate,
+    bytes32 claimedAuthmanIdHash);
   
   //key:ssnHash
   mapping(bytes32 => Authman) authmanBySsnHash;
   //key:guid
   mapping(bytes32 => Authman) authmanByGuid;
   //key:counter
-  mapping(uint => Authman) authmanByCounter;
+  mapping(bytes32 => Authman) authmanByAuthmanIdHash;
+  //key:claimHash
+  mapping(bytes32 => Authman) authmanByClaimHash;
 
   uint counter = 0;
 
+  //create or update authman
   function createOrUpdateAuthmanGuid(string firstName, 
-                                      string lastName, 
-                                      string ssn,
-                                      string dob, //YYYY-MM-DD
-                                      string pin, //4 digit
-                                      string mobilePhone, //10 digit
-                                      string requestedByName,
-                                      string requestedById) public {
+    string lastName, 
+    string ssn,
+    string dob, //YYYY-MM-DD
+    string pin, //4 digit
+    string mobilePhone, //10 digit
+    string requestedByName,
+    string requestedById) public {
 
     //validate ssn
     if (!validateSsn(ssn)) {
@@ -125,178 +132,322 @@ contract Main
       authman.firstName = firstName;
       authman.lastName = lastName;
       authman.mobilePhone = mobilePhone;
-      authman.claimHash = createClaimHash(ssn, mobilePhone, pin);
+      authman.claimHash = createClaimHash(authman.ssnHash, mobilePhone, pin);
       authman.updateById = requestedById;
       authman.updateByName = requestedByName;
       authman.updateDate = now;
       
-      saveAuthman(authman, authman.counter, authman.ssnHash);
+      saveAuthman(authman);
 
       //InfoHash("Updated authman.", authman.guid);
       logAuthman("Updated authman:", authman);
 
-    } else {
-      //if new
-      Authman memory newAuthman;
+      } else {
+        //if new
+        Authman memory newAuthman;
 
-      newAuthman.firstName = firstName;
-      newAuthman.lastName = lastName;
-      newAuthman.mobilePhone = mobilePhone;
-      newAuthman.claimHash = createClaimHash(ssn, mobilePhone, pin);
-      newAuthman.createById = requestedById;
-      newAuthman.createByName = requestedByName;
-      newAuthman.createDate = now;
+        newAuthman.firstName = firstName;
+        newAuthman.lastName = lastName;
+        newAuthman.mobilePhone = mobilePhone;
+        
+        newAuthman.createById = requestedById;
+        newAuthman.createByName = requestedByName;
+        newAuthman.createDate = now;
+        newAuthman.counter = counter;
+        newAuthman.ssnHash = hash;
+        newAuthman.guid = createGuid(newAuthman.ssnHash,newAuthman.counter);
+        newAuthman.claimHash = createClaimHash(newAuthman.guid, newAuthman.mobilePhone, pin);
 
-      if (counter != 0) {
-        newAuthman.previousGuid = authmanByCounter[counter].guid;
+        saveAuthman(newAuthman);
+        counter++;
+
+        //InfoHash("Saved new authman.", authman.guid);
+        logAuthman("New authman:", newAuthman);
       }
-      counter++;
-      newAuthman.counter = counter;
-      newAuthman.ssnHash = hash;
-      newAuthman.guid = createGuid(newAuthman.ssnHash,newAuthman.counter);
-      
-      saveAuthman(newAuthman, counter, hash);
-
-      //InfoHash("Saved new authman.", authman.guid);
-      logAuthman("New authman:", newAuthman);
-    }
-  }
-
-  function logAuthman(string message, Authman authman) private {
-    InfoAuthman1(message,
-                  authman.guid,
-                  authman.firstName,
-                  authman.lastName,
-                  authman.counter,
-                  authman.ssnHash,
-                  authman.claimHash,
-                  authman.mobilePhone
-      );
-    logAuthmanRest(authman);
-  }
-
-  function logAuthmanRest(Authman authman) private {
-    InfoAuthman2(  authman.createByName,
-                  authman.createDate,
-                  authman.updateByName,
-                  authman.updateDate,
-                  authman.claimedDate,
-                  authman.claimedAuthmanIdHash,
-                  authman.previousGuid
-      );
-  }
-  
-  function saveAuthman(Authman authman, uint counter1, bytes32 hash) private {
-      authmanByCounter[counter1] = authman;
-      authmanBySsnHash[hash] = authman;
-      authmanByGuid[authman.guid] = authman;
-  }
-
-  function validateSsn(string ssn) private returns (bool) {
-
-    //cannot use https://regex101.com/r/rP8wL0/1 ^(?!(000|666|9))\d{3}-(?!00)\d{2}-(?!0000)\d{4}$|^(?!(000|666|9))\d{3}(?!00)\d{2}(?!0000)\d{4}$
-
-    if (ssnRegex == address(0)) {
-      ssnRegex = new SsnRegex();
     }
 
-    SsnRegex s = SsnRegex(ssnRegex);
-    return s.matches(ssn);
+    //mobile phone is optional as banks may not have this information for current customers
+    function createOrUpdateAuthmanGuid(
+      string firstName, 
+      string lastName, 
+      string ssn,
+      string dob, //YYYY-MM-DD
+      string pin, //4 digit (auto generated by banks, when customer is not present)
+      string requestedByName,
+      string requestedById) public {
 
-  }
-
-  function validateDob(string dob) private returns (bool) {
-
-    if (dobRegex == address(0)) {
-      dobRegex = new DobRegex();
+      createOrUpdateAuthmanGuid(firstName, 
+        lastName, 
+        ssn, 
+        dob, 
+        pin, 
+        "", //no mobile phone
+        requestedByName, 
+        requestedById);
     }
 
-    DobRegex s = DobRegex(dobRegex);
-    return s.matches(dob);
+    //claiming authman from phone
+    function claimAuthmanId(
+      string authmanId,
+      string mobilePhone, 
+      string pin, 
+      bytes32 claimHash) public {
 
-  }
+      //validate phone 10 digits [1-9][0-9]{9}
+      if (!validatePhone(mobilePhone)) {
+        AnyException("Mobile phone is not valid. Should be 10 digits with no other characters.");
+        return;
+      }
 
-  function validatePhone(string phone) private returns (bool) {
+      //validate pin 4 digits [0-9]{4}
+      if (!validatePin(pin)) {
+        AnyException("Pin is not valid. Should be 4 digits.");
+        return;
+      }
 
-    if (phoneRegex == address(0)) {
-      phoneRegex = new PhoneRegex();
+      if (!validateAuthmanId(authmanId)) {
+        return;
+      }
+
+      bytes32 authmanIdHash = keccak256(authmanId);
+
+      if (authmanByAuthmanIdHash[authmanIdHash].guid[0] != 0) {
+        AnyException("Authman Id already in use. Try new authman Id.");
+        return;
+      }
+
+      Authman memory authman = authmanByClaimHash[claimHash];
+
+      if (authman.guid[0] == 0) {
+        AnyException("Claim hash is not valid. No corresponding authman found.");
+        return;
+      }
+
+      if (authman.isClaimed) {
+        AnyException("Authman is already claimed.");
+        return;
+      }
+
+      bytes32 newClaimHash;
+      bool mobilePhoneExists = bytes(authman.mobilePhone).length > 0;
+
+      //if mobilePhone exists
+      if (mobilePhoneExists) {
+        newClaimHash = createClaimHash(authman.guid, mobilePhone, pin);
+        } else {
+          newClaimHash = createClaimHash(authman.guid, "", pin);
+        }
+
+        if (newClaimHash != claimHash) {
+          AnyException("Mobile phone or pin is not valid.");
+          return;
+        }    
+
+        //valid credentials
+        authman.isClaimed = true;
+        authman.claimedDate = now;
+        authman.claimedAuthmanIdHash = authmanIdHash;
+        saveAuthman(authman);
+
+        logAuthman("Claimed authman.", authman);
+
+      }
+
+      function logAuthman(string message, Authman authman) private {
+        InfoAuthman1(message,
+          authman.guid,
+          authman.firstName,
+          authman.lastName,
+          authman.counter,
+          authman.ssnHash,
+          authman.claimHash,
+          authman.mobilePhone
+          );
+        logAuthmanRest(authman);
+      }
+
+      function logAuthmanRest(Authman authman) private {
+        InfoAuthman2(  authman.createByName,
+          authman.createDate,
+          authman.updateByName,
+          authman.updateDate,
+          authman.claimedDate,
+          authman.claimedAuthmanIdHash
+          );
+      }
+
+      function saveAuthman(Authman authman) private {
+        authmanBySsnHash[authman.ssnHash] = authman;
+        authmanByGuid[authman.guid] = authman;
+
+        if (authman.claimHash[0] > 0) {
+          authmanByClaimHash[authman.claimHash] = authman;
+        }
+        if (authman.claimedAuthmanIdHash[0] > 0) {
+          authmanByAuthmanIdHash[authman.claimedAuthmanIdHash] = authman;
+        }
+      }
+
+      function validateSsn(string ssn) private returns (bool) {
+
+        //cannot use https://regex101.com/r/rP8wL0/1 ^(?!(000|666|9))\d{3}-(?!00)\d{2}-(?!0000)\d{4}$|^(?!(000|666|9))\d{3}(?!00)\d{2}(?!0000)\d{4}$
+
+        if (ssnRegex == address(0)) {
+          ssnRegex = new SsnRegex();
+        }
+
+        SsnRegex s = SsnRegex(ssnRegex);
+        return s.matches(ssn);
+
+      }
+
+      function validateDob(string dob) private returns (bool) {
+
+        if (dobRegex == address(0)) {
+          dobRegex = new DobRegex();
+        }
+
+        DobRegex s = DobRegex(dobRegex);
+        return s.matches(dob);
+
+      }
+
+      function validatePhone(string phone) private returns (bool) {
+
+        if (phoneRegex == address(0)) {
+          phoneRegex = new PhoneRegex();
+        }
+
+        PhoneRegex s = PhoneRegex(phoneRegex);
+        return s.matches(phone);
+
+      }
+
+      function validatePin(string pin) private returns (bool) {
+
+        if (pinRegex == address(0)) {
+          pinRegex = new PinRegex();
+        }
+
+        PinRegex s = PinRegex(pinRegex);
+        return s.matches(pin);
+
+      }
+
+    /*
+    6 - 20 characers, 
+    all lower case, 
+    at least one alphabet, 
+    numbers are allowed but optional, 
+    one period is allowed in the middle but optional
+    */
+
+    function validateAuthmanId(string authmanId) private returns (bool) {
+
+      var authIdSlice = authmanId.toSlice();
+
+      if (authIdSlice.len() < 6 || authIdSlice.len() > 20) {
+        AnyException("Authman Id should be 6 - 20 characters.");
+        return false;
+      }
+
+      //split authmanId by period
+      var delim = ".".toSlice();
+      if (authIdSlice.count(delim) > 1) {
+        AnyException("Authman Id can only have one period. Period is optional.");
+        return false;
+      }
+
+      //lowerAlphaStartsWithAlphaRegex ensures that it is lower case alphanumerci beginning with an alphabet
+      //lowerAlphaRegex ensures that it is lower case alphanumerci beginning with an alphabet
+
+      if (lowerAlphaStartsWithAlphaRegex == address(0)) {
+        lowerAlphaStartsWithAlphaRegex = new LowerAlphanumericStartsWithAlphabetRegex();
+      }
+
+      LowerAlphanumericStartsWithAlphabetRegex id1 = LowerAlphanumericStartsWithAlphabetRegex(lowerAlphaStartsWithAlphaRegex);
+
+      if (authIdSlice.count(delim) == 0) {
+        //no period
+        return id1.matches(authmanId);
+
+        } else {
+          //two parts
+
+          if (lowerAlphaRegex == address(0)) {
+            lowerAlphaRegex = new LowerAlphanumericRegex();
+          }
+
+          LowerAlphanumericRegex id2 = LowerAlphanumericRegex(lowerAlphaRegex);
+
+          string memory part1 = authIdSlice.split(delim).toString();
+          string memory part2 = authIdSlice.split(delim).toString();
+
+          return (id1.matches(part1) && id2.matches(part2));      
+
+        }
+
+      }
+
+      //there is no random guid in solidity
+      function createGuid(bytes32 hash, uint counter1) private returns (bytes32) {
+        string memory guid = myString.bytes32ToString(hash).toSlice().concat(myString.uintToBytes(counter1).toSlice());
+        return keccak256(guid);
+      }
+
+      function createClaimHash(bytes32 hash, string phone, string pin) private returns (bytes32) {
+        return keccak256(myString.strConcat(myString.bytes32ToString(hash), phone, pin));
+      }
+
+      function createSsnHash(string ssn, string dob) private returns (bytes32) {
+
+        //removing dashes from ssn if any
+        string memory newSsn = removeDashesFromSsn(ssn);
+
+        //split ssn into 3 parts of 3 chars
+        string [] memory ssnArr = new string[](3);
+        ssnArr[0] = myString.substring(newSsn, 0, 3);
+        ssnArr[1] = myString.substring(newSsn, 3, 6);
+        ssnArr[2] = myString.substring(newSsn, 6, 9);
+
+        //split dob by dashes
+        var dobSlice = dob.toSlice();
+        var delim = "-".toSlice();
+        string memory year = dobSlice.split(delim).toString();
+        string memory  month = dobSlice.split(delim).toString();
+        string memory  day = dobSlice.split(delim).toString();
+
+        string memory pipe = "|";
+        string memory tilde = "~";
+
+        //concat them like YYYY|S1~MM|S2~DD|N3
+        string memory untilMonth = myString.strConcat(year, pipe, ssnArr[0], tilde, month);
+        string memory untilDay = myString.strConcat(untilMonth, pipe, ssnArr[1], day);
+        string memory hash = myString.strConcat(untilDay, pipe, ssnArr[2]);
+
+        //SSN+DOB keccak256
+        return keccak256(hash);
+
+      }
+
+      function removeDashesFromSsn(string ssn) private returns (string) {
+
+        //removing dashes from ssn if any
+        var s = ssn.toSlice();
+        var delim = "-".toSlice();
+        string memory newSsn = "";
+        for(uint i = 0; i < s.count(delim) + 1; i++) {
+          newSsn = myString.strConcat(ssn, s.split(delim).toString());
+        }
+        return newSsn;
+
+      }
+
+      function getCounter() public returns (uint) {
+        return counter;
+      }
+
     }
-
-    PhoneRegex s = PhoneRegex(phoneRegex);
-    return s.matches(phone);
-
-  }
-
-  function validatePin(string pin) private returns (bool) {
-
-    if (pinRegex == address(0)) {
-      pinRegex = new PinRegex();
-    }
-
-    PinRegex s = PinRegex(pinRegex);
-    return s.matches(pin);
-
-  }
-
-  //there is no random guid in solidity
-  function createGuid(bytes32 hash, uint counter1) private returns (bytes32) {
-    string memory guid = myString.bytes32ToString(hash).toSlice().concat(myString.uintToBytes(counter1).toSlice());
-    return keccak256(guid);
-  }
-
-  function createClaimHash(string ssn, string phone, string pin) private returns (bytes32) {
-    return keccak256(myString.strConcat(ssn, phone, pin));
-  }
-
-  function createSsnHash(string ssn, string dob) private returns (bytes32) {
-
-    //removing dashes from ssn if any
-    string memory newSsn = removeDashesFromSsn(ssn);
-
-    //split ssn into 3 parts of 3 chars
-    string [] memory ssnArr = new string[](3);
-    ssnArr[0] = myString.substring(newSsn, 0, 3);
-    ssnArr[1] = myString.substring(newSsn, 3, 6);
-    ssnArr[2] = myString.substring(newSsn, 6, 9);
-
-    //split dob by dashes
-    var dobSlice = dob.toSlice();
-    var delim = "-".toSlice();
-    string memory year = dobSlice.split(delim).toString();
-    string memory  month = dobSlice.split(delim).toString();
-    string memory  day = dobSlice.split(delim).toString();
-
-    string memory pipe = "|";
-    string memory tilde = "~";
-
-    //concat them like YYYY|S1~MM|S2~DD|N3
-    string memory untilMonth = myString.strConcat(year, pipe, ssnArr[0], tilde, month);
-    string memory untilDay = myString.strConcat(untilMonth, pipe, ssnArr[1], day);
-    string memory hash = myString.strConcat(untilDay, pipe, ssnArr[2]);
-    
-    //SSN+DOB keccak256
-    return keccak256(hash);
-  
-  }
-
-  function removeDashesFromSsn(string ssn) private returns (string) {
-
-    //removing dashes from ssn if any
-    var s = ssn.toSlice();
-    var delim = "-".toSlice();
-    string memory newSsn = "";
-    for(uint i = 0; i < s.count(delim) + 1; i++) {
-        newSsn = myString.strConcat(ssn, s.split(delim).toString());
-    }
-    return newSsn;
-
-  }
-
-  function getCounter() public returns (uint) {
-    return counter;
-  }
-
-}
 
   /*
 
---
+++ contracts/helper/LowerAlphanumericStartsWithAlphabetRegex.sol
@@ -0,0 +1,59 @@
+pragma solidity ^0.4.11;
+
+contract LowerAlphanumericStartsWithAlphabetRegex {
+  struct State {
+    bool accepts;
+    function (byte) constant internal returns (State memory) func;
+  }
+
+  string public constant regex = "[a-z][a-z0-9]+";
+
+  function s0(byte c) constant internal returns (State memory) {
+    c = c;
+    return State(false, s0);
+  }
+
+  function s1(byte c) constant internal returns (State memory) {
+    if (c >= 97 && c <= 122) {
+      return State(false, s2);
+    }
+
+    return State(false, s0);
+  }
+
+  function s2(byte c) constant internal returns (State memory) {
+    if (c >= 48 && c <= 57 || c >= 97 && c <= 122) {
+      return State(true, s3);
+    }
+
+    return State(false, s0);
+  }
+
+  function s3(byte c) constant internal returns (State memory) {
+    if (c >= 48 && c <= 57 || c >= 97 && c <= 122) {
+      return State(true, s4);
+    }
+
+    return State(false, s0);
+  }
+
+  function s4(byte c) constant internal returns (State memory) {
+    if (c >= 48 && c <= 57 || c >= 97 && c <= 122) {
+      return State(true, s4);
+    }
+
+    return State(false, s0);
+  }
+
+  function matches(string input) constant returns (bool) {
+    var cur = State(false, s1);
+
+    for (uint i = 0; i < bytes(input).length; i++) {
+      var c = bytes(input)[i];
+
+      cur = cur.func(c);
+    }
+
+    return cur.accepts;
+  }
+}
